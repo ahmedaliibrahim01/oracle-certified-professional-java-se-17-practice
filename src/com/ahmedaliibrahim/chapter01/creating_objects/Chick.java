0f5cf505cfe67bb51f065292cc60dc70cab31c15
Ahmed Ali IBRAHIM <ahmedaliibrahim01@gmail.com>
2024-06-06 21:19:41 +0300

    creating objects

---
+++ src/com/ahmedaliibrahim/chapter01/creating_objects/Chick.java
@@ -0,0 +1,9 @@
+package com.ahmedaliibrahim.chapter01.creating_objects;
+
+public class Chick {
+    public Chick() {
+        System.out.println("in constructor");
+    }
+
+    public void Chick() { } // NOT A CONSTRUCTOR
+}
